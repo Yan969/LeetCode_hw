41e728a1aec1431dab6343f59600480ccced5d55
root <2729863621@qq.com>
2021-04-11 23:15:00 +0800

    week02

---
+++ week02/1.两数之和.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	ht := make(map[int]int)
+	for i, v := range nums{
+		if p, ok := ht[target-v]; ok{
+			return []int{p, i}
+		}
+		ht[v]=i
+	}
+	return nil
+}
+// @lc code=end
+
